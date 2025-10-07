d3fdfedaad4cc1da3b75952eb109b9dad5b74562
wiktor <kowalinski.wiktor04@gmail.com>
2025-10-07 22:07:17 +0200

    Add configuration files

---
+++ migrations/Version20250830173004.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250830173004 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE short_links (
+            id UUID NOT NULL,
+            slug VARCHAR(255) NOT NULL,
+            url VARCHAR(255) NOT NULL,
+            count INT NOT NULL,
+            PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE short_links');
+    }
+}
